9e0c8f58e6be5139d14cfa3e789da4929a0b5256
SpitfireX <niiiioouuuuuu@gmail.com>
2013-01-18 23:03:40 +0100

    Revert to original version

--- main/robot.py
+++ main/robot.py
@@ -1,11 +1,8 @@
-from sr_emulator import *
-#from motor_control import *
-import threading, time
+from sr import *
+from motor_control import *
+import os
 
 r = Robot()
-
-thread = None
-instructions = []
 
 def initRobot():
     print "Initializing Robot"
@@ -16,53 +13,7 @@ def initRobot():
     
 #def startEventLoop():
 
-################################################################
-################################################################
-
-class MotorCotrolThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        print "Started MotorControlThread"
-        
-        while True:
-            i = instructions[0]
-            
-            if len(instructions) == 0:
-                print "no instructions"
-                for m in r.motors:
-            		m.target=0
-            else:
-                index = 0
-                
-                for m in i.motors:
-                    print "instruction:", i.motors, i.speeds, i.duration
-                    m.target=i.speeds[index]
-                    index += 1
-                    
-	            time.sleep(i.duration)
-	            instructions = instructions[1:len(instructions)]
-
-class MotorInstruction():
-    motors = []
-    speeds = []
-    duration = None
-    
-    def _init_(self, motors, speeds, duration = 0):
-        self.motors = motors
-        self.speeds = speeds
-        self.duration = duration
-
-def initMotorControl():
-    print "Initializing MotorControl"
-    thread = MotorCotrolThread()
-    thread.start()
-    
-def addMotorInstruction(motors, speeds, duration = 0):
-    i = MotorInstruction(motors, speeds, duration)
-    instructions.append(i)
-    
+print os.getcwd()
 print dir()
 initRobot()
 addMotorInstruction(r.motors, [80, 80], 5)
