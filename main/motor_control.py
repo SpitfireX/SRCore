6373117a103533c7e14feab27110f6287fc34447
SpitfireX <niiiioouuuuuu@gmail.com>
2013-01-19 19:03:24 +0100

    Rewritten motor_control and some other stuff

--- main/motor_control.py
+++ main/motor_control.py
@@ -3,51 +3,79 @@ import time
 import threading
 
 thread = None
-r = None
+running = False
 
-class MotorCotrolThread(threading.Thread):
-    instructions = []
-    
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        print "Started MotorControlThread"
-        
-        while True:
-            i = instructions[0]
-            
-            if len(instructions) == 0:
-                print "no instructions"
-                for m in r.motors:
-                        m.target = 0
-            else:
-                index = 0
-                
-                for m in i.motors:
-                    print "instruction:", i.motors, i.speeds, i.duration
-                    m.target = i.speeds[index]
-                    index += 1
-                    
-                time.sleep(i.duration)
-                instructions = instructions[1:len(instructions)]
-
-class MotorInstruction():
-    motors = []
-    speeds = []
-    duration = None
-    
-    def _init_(self, motors, speeds, duration = 0):
-        self.motors = motors
-        self.speeds = speeds
-        self.duration = duration
+instructions = []
 
 def initMotorControl(robot):
     print "Initializing MotorControl"
     r = robot
-    thread = MotorCotrolThread()
+    thread = MotorCotrolThread(r)
     thread.start()
     
 def addMotorInstruction(motors, speeds, duration = 0):
     i = MotorInstruction(motors, speeds, duration)
     instructions.append(i)
+    
+def skipCurrentInstruction():
+    print thread
+    thread.skip()
+
+class MotorCotrolThread(threading.Thread):
+    
+    cI = None
+    
+    def __init__(self, robot):
+        threading.Thread.__init__(self)
+        self.r = robot
+
+    def run(self):
+        print "Started MotorControlThread"
+        running = True
+        
+        while running:
+            if len(instructions) > 0:
+                cI = instructions.pop(0)
+                cI.setup()
+                cI.run()
+            else:
+                print "no instructions"
+                cI = None
+                for m in self.r.motors:
+                    m.target = 0
+        
+    def skip(self):
+        if self.cI != None:
+            self.cI.skip()
+                
+class MotorInstruction():
+    def __init__(self, motors, speeds, duration = 0):
+        self.motors = motors
+        self.speeds = speeds
+        self.duration = duration
+        self.skipped = False
+        
+    def setup(self):
+        for i in range(len(self.motors)):
+            self.motors[i].target = self.speeds[i]
+    
+    def run(self):
+        print "running for", self.duration, "seconds"
+        lasttime = time.time()
+        
+        if self.duration > 0:
+            while not self.skipped:
+                now = time.time()
+                diff = now - lasttime
+                self.duration -= diff
+            
+                if self.duration <= 0:
+                    break
+            
+                lasttime = now
+        else:
+            while not self.skipped:
+                time.sleep(0.01)
+            
+    def skip(self):
+        self.skipped = True
--- main/robot.py
+++ main/robot.py
@@ -1,5 +1,6 @@
 from sr_emulator import *
 import motor_control
+import time
 
 robot = Robot()
 
@@ -7,6 +8,7 @@ def initRobot():
     print "Initializing Robot"
     motor_control.initMotorControl(robot)
     print "Finished robot initializsation"
+    
     startEventLoop()
     
 def startEventLoop():
@@ -14,8 +16,10 @@ def startEventLoop():
 
 initRobot()
 
-motor_control.addMotorInstruction(robot.motors, [80, 80], 5)
-motor_control.addMotorInstruction(robot.motors, [-80, -80], 5)
-motor_control.addMotorInstruction(robot.motors, [20, 60], 2)
-motor_control.addMotorInstruction(robot.motors, [60, 20], 2)
+#motor_control.addMotorInstruction(robot.motors, [80, 80], 5)
+#motor_control.addMotorInstruction(robot.motors, [-80, -80], 5)
+#motor_control.addMotorInstruction(robot.motors, [20, 60], 2)
+#motor_control.addMotorInstruction(robot.motors, [60, 20], 2)
 motor_control.addMotorInstruction(robot.motors, [80, 80], 0)
+time.sleep(2)
+motor_control.skipCurrentInstruction()
