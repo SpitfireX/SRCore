cc8910458b6668bfdcfde91f732fd3ff0a653fd2
kaywest <maximilian.hebeis@gmx.de>
2013-01-20 03:25:58 +0100

    Update main/logger.py
    Added opportunity to save the log in a text file (log.txt)

--- main/logger.py
+++ main/logger.py
@@ -9,7 +9,11 @@ def log(message):
     timestamp = strftime("%H:%M:%S %d.%m.%Y ") 
     timestamp += current_thread().name
     timestamp += ":"
-
+    
     print timestamp, message
+    
+    logfile=open("log.txt", "a"); # Nur rin Vorschlag, um den Log auch in einer Datei auf dem USB-Stick
+    logfile.write(timestamp+" "+message) #zu haben. Könnte nützlich sein, um später Fehler zu checken o.ä.
+    logfile.close() #ihr könnt das aber auch auskommentieren, wenn ihr wollt. -Max
     
     thread_lock.release()
