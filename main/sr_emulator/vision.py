14ef9941c8443df2c1c40b1d4579ced70e74416d
kaywest <maximilian.hebeis@gmx.de>
2013-01-24 09:02:48 -0800

    Create vision.py
    Emulating the vision-class

---
+++ main/sr_emulator/vision.py
@@ -0,0 +1,34 @@
+import random, time, collections
+
+random.seed()
+
+MARKER_ARENA, MARKER_ROBOT, MARKER_PEDESTAL, MARKER_TOKEN = range(0,4)
+Marker=collections.namedspace("MarkerBasis", "info dist rot_y");
+MarkerInfo=collections.namedspace("MarkerInfo", "code marker_type");
+  
+class Vision():
+	
+	def see(self):
+		i=random.randint(0, 5)
+		dist=random.randint(0, 8)
+		
+		if i == 0:
+			return []
+		
+		else:
+			elif i == 1:
+				inf = MarkerInfo(random.randint(0, 27), MARKER_ARENA)
+		
+			elif i == 2:
+				inf = MarkerInfo(random.randint(28, 31), MARKER_ARENA)
+		
+			elif i == 3:
+				inf = MarkerInfo(random.randint(32, 40), MARKER_ARENA)
+		
+			elif i == 4:
+				inf = MarkerInfo(random.randint(41, 64), MARKER_ARENA)
+				
+			marker = Marker(inf, dist, random.randint(-180, 180))
+			return [marker]
+	
+		time.sleep(0.5)
