69cfca59f6f77ef74ecfd1520e8abfd78c57d4c5
kaywest <maximilian.hebeis@gmx.de>
2013-01-26 19:58:38 +0100

    Update main/calibrate.py

--- main/calibrate.py
+++ main/calibrate.py
@@ -7,16 +7,22 @@ def calibrate():
 		t1 = time.time()
 		duration=1
 		markers = R.see()
-		while duration > 0:
-			duration -= time.time() - t1
+		if len(markers)!=0:
+			R.motors[0:].target = pwr
+			while duration > 0:
+				duration -= time.time() - t1
+			
+			R.motors.target = 0
+			markers2 = R.see()
 		
-		markers2 = R.see()
-		
-		gefahren = markers[0].dist - markers2[0].dist
-		print "Roboter fährt pro Sekunde mit", pwr, "Prozent Motorleistung", gefahren, "Meter"
-		v.append(gefahren)
+			gefahren = markers[0].dist - markers2[0].dist
+			print "Roboter fährt pro Sekunde mit", pwr, "Prozent Motorleistung", gefahren, "Meter"
+			v.append(gefahren)
 	
-	return v
+			return v
+		else:
+			print "Bitte den Roboter richtig ausrichten!"
+			break
 	
 if __name__=='__main__':
 	calibrate()
