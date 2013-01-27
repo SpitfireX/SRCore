43b9f9e16734c7b13f581a6293aab5d61fa31935
kaywest <maximilian.hebeis@gmx.de>
2013-01-27 12:39:44 +0100

    Moved some piece of code to marker_text.txt

--- main/sensor_control.py
+++ main/sensor_control.py
@@ -18,26 +18,6 @@ class MarkerThread(threading.Thread):
     def run(self):
         log("Started MarkerThread")
 
-        global r
-        global changes
-
-        pedestals = {32:Coordinate(2000, 6000),
-                     33:Coordinate(4000, 6000),
-                     34:Coordinate(6000, 6000),
-                     35:Coordinate(2000, 4000),
-                     36:Coordinate(4000, 4000),
-                     37:Coordinate(6000, 4000),
-                     38:Coordinate(2000, 2000),
-                     39:Coordinate(4000, 2000),
-                     40:Coordinate(6000, 2000)}
-
-        m_info = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:7,
-                  27:1, 26:2, 25:3, 24:4, 23:5, 22:6, 21:7,
-                  14:1, 15:2, 16:3, 17:4, 18:5, 19:6, 20:7,
-                  7:1, 8:2, 9:3, 10:4, 11:5, 12:6, 13:7}
-
-        x = y = None # Position des Roboters.
-
         while True:
             markers = r.see()
 
