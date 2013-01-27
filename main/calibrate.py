f2f27eb64740794389104236c1b61b71bb4c1458
kaywest <maximilian.hebeis@gmx.de>
2013-01-27 15:39:40 +0100

    Update main/calibrate.py

--- main/calibrate.py
+++ main/calibrate.py
@@ -5,17 +5,13 @@ def calibrate():
   v=[]
 	for pwr in range(10, 80, 10):
 		t1 = time.time()
-		duration=1
 		markers = R.see()
-		if len(markers)!=0:
-			R.motors[0].target = pwr
-			R.motors[1].target = pwr
-			while duration > 0:
-				duration -= time.time() - t1
-			
-			R.motors[0].target = 0
-			R.motors[1].target = 0
-			markers2 = R.see()
+		R.motors[0].target = pwr
+		R.motors[1].target = pwr
+		R.motors[0].target = 0
+		R.motors[1].target = 0
+		t2 = time.time()
+		markers2 = R.see()
 		
 			gefahren = markers[0].dist - markers2[0].dist
 			print "Roboter fährt pro Sekunde mit", pwr, "Prozent Motorleistung", gefahren, "Meter"
