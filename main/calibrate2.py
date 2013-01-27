e88cc0d1c8b73e38f401643276f966168a8223ee
SpitfireX <niiiioouuuuuu@gmail.com>
2013-01-27 16:06:02 +0100

    Added my own calibratino module

---
+++ main/calibrate2.py
@@ -0,0 +1,13 @@
+from sr_emulator import *
+from logger import log
+import time
+import motor_control
+
+def calibrate2(robot):
+    motor_control.addMotorInstruction(robot.motors, [100,100], 0)
+    t = time.time()
+    robot.see()
+    motor_control.skipCurrentInstruction()
+    t2 = time.time()
+    
+    log(t2 - t)
--- main/robot.py
+++ main/robot.py
@@ -1,6 +1,6 @@
 from sr_emulator import *
 from logger import log
-import motor_control, sensor_control
+import motor_control, sensor_control, calibrate2
 import time
 
 robot = Robot()
@@ -8,10 +8,10 @@ robot = Robot()
 def initRobot():
     log("Initializing Robot")
     
-    sensor_control.initSensorControl(robot)
+    #sensor_control.initSensorControl(robot)
     motor_control.initMotorControl(robot)
     
-    sensor_control.startThread()
+    #sensor_control.startThread()
     motor_control.startThread()
     log("Finished robot initializsation")
     
@@ -27,7 +27,8 @@ initRobot()
 #motor_control.addMotorInstruction(robot.motors, [20, 60], 2)
 #motor_control.addMotorInstruction(robot.motors, [60, 20], 2)
 
-motor_control.addMotorInstruction(robot.motors, [80, 80], 0)
-time.sleep(2)
-motor_control.skipCurrentInstruction()
-motor_control.addMotorInstruction(robot.motors, [-80, -80], 5)
+#motor_control.addMotorInstruction(robot.motors, [80, 80], 0)
+#time.sleep(2)
+#motor_control.skipCurrentInstruction()
+#motor_control.addMotorInstruction(robot.motors, [-80, -80], 5)
+calibrate2.calibrate2(robot)
