3e0a3e9b8d19a76d3fd7d09f742ebd5372f66682
SpitfireX <niiiioouuuuuu@gmail.com>
2013-02-09 16:41:50 +0100

    Added logic package Minor changes to logic_control.py

--- main/logic_control.py
+++ main/logic_control.py
@@ -2,6 +2,7 @@ from sr_emulator import *
 from logger import log
 
 def processEvents(events):
+    log("Events: " + len(events))
     for event in events:
         if type(event) == Marker:
             log("Marker: " + str(event))
