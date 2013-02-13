379e0e33feedb08f96486a383cbac40bd5c7527f
kaywest <maximilian.hebeis@gmx.de>
2013-02-13 23:19:34 +0100

    Completed calibrate module

--- main/calibrate.py
+++ main/calibrate.py
@@ -1,26 +1,46 @@
 from sr import *
 import motor_control
-import time
 
 def calibrate():
-  v=[]
-  R=Robot()
+	v=[]
+	w=[]
+	R=Robot()
 	for pwr in range(10, 80, 10):
-		t1 = time.time()
 		markers = R.see()
 		motor_control.addMotorInstruction(R.motors, [pwr, pwr], 1)
-		t2 = time.time()
 		markers2 = R.see()
 		
+		if len(markers) != 0 and len(markers2) != 0:
 			gefahren = markers[0].dist - markers2[0].dist
 			print "Roboter fährt pro Sekunde mit", pwr, "Prozent Motorleistung", gefahren, "Meter"
 			v.append(gefahren)
-	
-			return v
+		
 		else:
 			print "Bitte den Roboter richtig ausrichten!"
 			break
+		
+		motor_control.addMotorInstruction(R.motors, [-pwr, -pwr], 1)
 	
-if __name__=='__main__':
-	calibrate()
 	
+	for pwr in range(10, 80, 10):
+		markers = R.see()
+		motor_control.addMotorInstruction(R.motors, [pwr, -pwr], 0.5)
+		markers2 = R.see()
+		
+		if len(markers) != 0 and len(markers2) != 0:
+			winkel = markers2[0].rot_y - markers[0].rot_y
+			print "Bei", pwr, "Prozent Motorleistung werden", winkel, "Grad zurueckgelegt."
+			w.append(winkel)
+		
+		else:
+			print "Bitte den Roboter richtig ausrichten!"
+			break
+		
+		print "Bitte Roboter ausrichten!"
+		wait_for(R.io[0].input[0].query.d == 1, R.io[0].input[1].query.d == 1)
+		
+	return v, w	
+
+if __name__ == '__main__':
+	v, w=calibrate()
+	print "v:", v, "\nw:", w	
