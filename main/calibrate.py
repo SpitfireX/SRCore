97a58a8f5e1205456cee42e242cbef0d47b6b151
kaywest <maximilian.hebeis@gmx.de>
2013-02-24 17:47:00 +0100

    Update main/calibrate.py

--- main/calibrate.py
+++ main/calibrate.py
@@ -32,7 +32,8 @@ def calibrate(r):
 	print "Weitere Kalibrierung startet auf Knopfdruck."
 	wait_for(r.io[0].input[0].query.d == 1, r.io[0].input[1].query.d ==1)
 
-	for pwr in range(10, 80, 10):
+	pwr=10
+	while True:
 		markers = r.see()
 		addMotorInstruction(r.motors, [pwr, -pwr], 1)
 		markers2 = r.see()
@@ -44,14 +45,19 @@ def calibrate(r):
 
 		else:
 			print "Bitte den roboter richtig ausrichten!"
-			break
+			continue
 
-		print "Bitte roboter ausrichten!"
-		wait_for(r.io[0].input[0].query.d == 1, r.io[0].input[1].query.d == 1)
+                if pwr == 80:
+                        break
+
+                else:
+                        print "Bitte roboter ausrichten!"
+                        wait_for(r.io[0].input[0].query.d == 1, r.io[0].input[1].query.d == 1)
+                        pwr+=10
 
 	return v, w
 
-def addMotorInstruction(motors, speeds, duration):
+def addMotorInstruction(self, motors, speeds, duration):
 	for i in range(len(motors)):
 		motors[i].target=speeds[i]
 	
