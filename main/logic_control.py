e968ee0eb66df3d03edfbe2f9d39d1555f19d25a
Lukas Braun <koomi@hackerspace-bamberg.de>
2013-02-24 15:57:09 +0100

    cast to str

--- main/logic_control.py
+++ main/logic_control.py
@@ -2,7 +2,7 @@ from sr_emulator import *
 from logger import log
 
 def processEvents(events):
-    log("Events: " + len(events))
+    log("Events: " + str(len(events)))
     for event in events:
         if type(event) == Marker:
             log("Marker: " + str(event))
