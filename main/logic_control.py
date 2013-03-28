e4ab2d9cea209eb49acaa39814ee7dc71782cf8b
kaywest <maximilian.hebeis@gmx.de>
2013-03-28 19:03:12 +0100

    Some bugs fixed

--- main/logic_control.py
+++ main/logic_control.py
@@ -1,10 +1,10 @@
 from sr_emulator import *
-from logger import log
+from logger import debug
 
 def processEvents(events):
     # log("Events: " + str(len(events)))
     for event in events:
         if event.eventType == "Marker":
-            log("Marker: " + str(event))
+            debug("Marker: " + str(event))
         # elif event.eventType == "Pin":
         #     log("Pin: "str(event))
