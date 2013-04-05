e192e8433e51b7522b426be331df3ae36718c7d1
Jonas Hoersch <coroa@online.de>
2013-04-06 01:10:11 +0200

    extend vector_math to work with arbitrary tuple sizes

--- main/vector_math.py
+++ main/vector_math.py
@@ -1,15 +1,13 @@
 from math import sqrt
 
 def vAdd(vec1, vec2):
-    return (vec1[0] + vec2[0],
-            vec1[1] + vec2[1],
-            vec1[2] + vec2[2])
+    return ( v1 + v2 for v1, v2 in zip(vec1, vec2) )
 
 def sProd(vec1, vec2):
-    return (vec1[0]*vec2[0] + vec1[1]*vec2[1] + vec1[2]*vec2[2])
+    return reduce(lambda s,v: s+v[0]*v[1], zip(vec1, vec2), 0)
 
 def sMult(num, vec):
-    return (vec[0]*num, vec[1]*num, vec[2]*num)
+    return ( num * v for v in vec )
 
 def vLen(vec):
-    return sqrt(vec[0]**2 + vec[1]**2 + vec[2]**2)
+    return sqrt(sProd(vec,vec))
