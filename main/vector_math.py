23ca0603569c5e9cdd075b905a945695c9813f0d
Lukas Braun <koomi@hackerspace-bamberg.de>
2013-04-11 17:44:24 +0200

    Use lists instead of generators (gens are unsubscriptable)

--- main/vector_math.py
+++ main/vector_math.py
@@ -1,13 +1,13 @@
 from math import sqrt
 
 def vAdd(vec1, vec2):
-    return ( v1 + v2 for v1, v2 in zip(vec1, vec2) )
+    return [ v1 + v2 for v1, v2 in zip(vec1, vec2) ]
 
 def sProd(vec1, vec2):
     return reduce(lambda s,v: s+v[0]*v[1], zip(vec1, vec2), 0)
 
 def sMult(num, vec):
-    return ( num * v for v in vec )
+    return [ num * v for v in vec ]
 
 def vLen(vec):
-    return sqrt(sProd(vec,vec))
+    return sqrt(abs(sProd(vec,vec)))
