d6882e84dd8127070b1b13fb3e5f8d9b8bbfbc4c
kaywest <maximilian.hebeis@gmx.de>
2013-04-14 14:09:00 +0200

    Update robot.py

--- main/robot.py
+++ main/robot.py
@@ -1,8 +1,13 @@
+# To do:
+# - ownTokens in strategy.py
+# - Funktioniert addAngleInstruction und approachMarker jetzt richtig??
+# - UNBEDINGT: Testen der eigenen Koordinaten-Bestimmung
+# - EBENFALLS UNEDINGT: Verstehen, wie DestinationObjective() funktioniert
 from sr import *
 from logger import debug
 from strategy import *
 from logic.computeChanges import *
-import motor_control, sensor_control, logic_control
+import motor_control, sensor_control, logic_control, servo_control
 import time
 
 robot = Robot()
@@ -13,26 +18,36 @@ def initRobot():
 
     sensor_control.initSensorControl(robot)
     motor_control.initMotorControl(robot)
-
+    motor_control.currentAngle = 0
     motor_control.startThread()
     initialActions()
     sensor_control.startThread()
+    servo_control.initServoControl(robot)
     debug("Finished robot initializsation")
 
 def initialActions():
     ctr = 0
     while True:
-        motor_control.addAngleInstruction(20)
-        markers = robot.see()
-    	for m in markers:
-            if m.info.marker_type == MARKER_ARENA and m.dist < 2100:
-                code = m.info.code
-                coor = m_info(code)
-                motor_control.currentAngle = 0 if code == 0 or code == 3 else 180
-				home = (coor[0]-m.centre.world.x, coor[1]-m.centre.world.y)
-				strategy.home = home
-                motor_control.addAngleInstruction(-ctr*20)
-                break
+        motor_control.addAngleInstruction(45)
+        time.sleep(2)
+        sensor_control.event.wait()
+        markers = sensor_control.getMarkers()
+        sensor_control.event.clear()
+        if len(markers)>0:
+            for m in markers:
+                if m.info.marker_type == MARKER_ARENA and m.dist < 2100:
+                    code = m.info.code
+                    strategy.homenumber = 0 if 0 <= code <= 6 else 1 if 21 <= code <= 27 else 2 if 14 <= code <= 20 else 3
+                    coor = m_info[code]
+                    homecoor = computeAbsolutePositionByArenaMarker(m)
+                    motor_control.currentAngle = homecoor[2]
+                    home = (homecoor[0], homecoor[1])
+                    strategy.home = home
+                    motor_control.addAngleInstruction(-ctr*30)
+                    debug("Token found!")
+                    break
+        else:
+            debug("No token found!")
         
         ctr += 1
 
@@ -40,17 +55,12 @@ def startEventLoop():
     debug("Starting event loop")
     global running
     running = True
-    strategy = Strategy(robot)
-
+    strategy = Strategy(robot) 
     while running:
         sensor_control.event.wait()
-        markers = sensor_control.getChanges()
+        markers = sensor_control.getMarkers()
         sensor_control.event.clear()
-        # debug("Markers: " + str(len(events)))
-        # strategy.act(events)
-        for m in markers:
-            if m.info.marker_type == MARKER_ARENA:
-              debug("Computed position:  " + str(computeAbsolutePositionByArenaMarker(m)))
+        strategy.reach(markers)
 
 def stopEventLoop():
     global running
