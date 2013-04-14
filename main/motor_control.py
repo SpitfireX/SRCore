16a1ad75212fa324a6c46c2818e5810f9de7dc33
kaywest <maximilian.hebeis@gmx.de>
2013-04-14 14:02:29 +0200

    Improved (and ugly) new motor_control

--- main/motor_control.py
+++ main/motor_control.py
@@ -1,6 +1,6 @@
 from sr_emulator import *
 from logger import debug
-import threading
+import threading, time
 from logic.computeChanges import computeCoordinates
 from math import *
 
@@ -11,81 +11,51 @@ global changePoints
 currentAngle = 0
 changePoints = []
 
-class MotorCotrolThread(threading.Thread):
-    cI = None
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.name = "MotorControl"
-
-    def run(self):
-        debug("Started MotorControl Thread")
-
-        global running
-        running = True
-
-        while running:
-            global cI
-            if len(instructions) > 0:
-                cI = instructions.pop(0)
-                cI.setup()
-                cI.run()
-            else:
-                #debug("no instructions")
-                cI = None
-                for m in r.motors:
-                    m.target = 0
-
-    def skip(self):
-        if cI is not None:
-            cI.skipped = True
-            debug("    skipping instruction")
-        else:
-            debug("    nothing to skip")
-
 class MotorInstruction():
     def __init__(self, speeds, ticks = 0):
         if (len(speeds) != 2): raise Exception("speeds needs to be a 2-list")
-        if (not (ticks > 0)): raise Exception("ticks needs to be positive")
+        #if (not (ticks > 0)): raise Exception("ticks needs to be positive")
         self.speeds = speeds
         self.ticks = ticks
-        self.skipped = False
-
-    def setup(self):
-        global r
-        r.motors[0].target = self.speeds[0]
-        r.motors[1].target = self.speeds[1]
 
     def run(self):
+        debug("Running for " + str(self.ticks) + " ticks")
+        left.addInstruction(self.speeds[0], self.ticks)
+        right.addInstruction(self.speeds[1], self.ticks)
+
+class WheelTickCount(threading.Thread):
+    def __init__(self, mode):
+        threading.Thread.__init__(self)
+        self.mode = mode
+        self.cI = None
+        self.ticks = 0
+        self.skipped = False
+    	
+    def run(self):
         global r, allticks
-        if self.ticks > 0:
-            debug("Running for " + str(self.ticks) + " ticks")
-            rightTicks = leftTicks = 0
-
-            ticked = 2
-
-            while rightTicks < self.ticks and leftTicks < self.ticks:
-                if self.skipped:
-                    break
-
-                wait_for(r.io[0].input[ticked].query.d==0)
-                res = wait_for(r.io[0].input[2].query.d == 1, r.io[0].input[3].query.d == 1)
-                if res[1] != None:
-                    rightTicks += 1
-                    ticked = 3
-                    if r.motors[0].target>1 and r.motors[1].target>1:
-                        allticks += 1
-                else:
-                    leftTicks += 1
-                    ticked = 2
-                    if r.motors[0].target>1 and r.motors[1].target>1:
+        while True:
+            if self.cI != None:
+                r.motors[self.mode].target = self.cI[0]
+                while self.ticks < self.cI[1] and not self.skipped:
+                    if self.mode == 0:
+                        wait_for(r.io[0].input[2].query.d == 1)
+                    else:
+                        wait_for(r.io[0].input[3].query.d == 1)
+                    self.ticks+=1
+                    if r.motors[0].target>=1 and r.motors[1].target>=1:
                         allticks+=1
-        else:
-            debug("Running forever")
-            while not self.skipped:
-                time.sleep(0.1)
-        debug("Instruction done")
-
+                    time.sleep(0.02)
+                    r.motors[self.mode].target = 0
+                    self.cI=None
+                    self.skipped=False
+					
+    def addInstruction(self, speed, ticks):
+        self.cI=[speed, ticks]
+		
+    def skip():
+        self.skipped = True
+        debug("Skipped current Instruction")
+        
 def getCurrentAngle():
     global currentAngle
     return currentAngle
@@ -100,33 +70,35 @@ def initMotorControl(robot):
     global r
     r = robot
 
-    global thread
-    thread = MotorCotrolThread()
+    global left, right
+    left=WheelTickCount(1)
+    right=WheelTickCount(0)
 
     global running
     running = False
 
-    global v
-    global w
-    # v, w = calibrate(r)
-    # checkCalibrating()
-
 def startThread():
     global running
     running = True
-    thread.start()
+    left.start()
+    right.start()
 
 def stopThread():
     global running
     running = False
 
-def addMotorInstruction(speeds = [70, 70], ticks = 0):
-    i = MotorInstruction(speeds, ticks)
-    instructions.append(i)
+def addMotorInstruction(speeds = [80, 80], ticks = 0):
+    global left, right
+    if len(speeds) > 0:
+        left.addInstruction(speeds[1], ticks)
+        right.addInstruction(speeds[0], ticks)
+        debug(str(speeds)+" "+str(ticks))
 
 def skipCurrentInstruction():
+    global left, right
     debug ("Attempting to skip instruction")
-    thread.skip()
+    left.skip()
+    right.skip()
 
 def getCurrentInstruction():
     global cI
@@ -139,18 +111,18 @@ def getCurrentAngle():
 def addAngleInstruction(angle):
     global currentAngle
     global changePoints
-    ticks = angle/(90/9)
+    ticks = angle/(90/8)
     #print changePoints
-    # coor = computeCoordinates(allticks, changePoints, currentAngle)
-    # recentTicks = allticks if len(changePoints) == 0 else allticks - changePoints[len(changePoints)-1][2]
-    # changePoints.append([currentAngle, coor, recentTicks])
+    #coor = computeCoordinates(allticks, changePoints, currentAngle)
+    #recentTicks = allticks if len(changePoints) == 0 else allticks - changePoints[len(changePoints)-1][2]
+    #changePoints.append([currentAngle, coor, recentTicks])
     if ticks < 1:
         ticks = 1
-    speed = [70, -70] if angle < 0 else [-70, 70]
+    speed = [100, -100] if angle < 0 else [-100, 100]
     addMotorInstruction(speed, ticks)
     currentAngle += angle
 
-def addImmediateInstruction(speeds = [70,70], ticks = 0):
+def addImmediateInstruction(speeds = [100, 100], ticks = 0):
     global instructions
     instructions = []
     skipCurrentInstruction()
@@ -163,6 +135,6 @@ def addImmediateAngleInstruction(angle):
     addAngleInstruction(angle)
 
 def driveUntilPressed():
-    addMotorInstruction([30, 30], -1)
+    addMotorInstruction([70, 70], -1)
     wait_for(And(r.io[0].input[6].query.d == 1, r.io[0].input[7].query.d == 1))
     skipCurrentInstruction()
